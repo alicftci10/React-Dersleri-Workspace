c905d8dbc58c4f942586781ae5c8d46d161b553b
ALi <alicftci10@gmail.com>
2025-02-27 19:17:42 +0300

    redux kurulumu

---
+++ 11.redux/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './redux/counterSlice'
+import UserList from './UserList'
+
+function App() {
+
+  // const dispatch = useDispatch();
+  // const { value } = useSelector((store) => store.counter);
+  // console.log(value)
+
+  return (
+    // <div>
+    //   <div>{value}</div>
+    //   <div><button onClick={() => dispatch(increment())}>Arttır</button>
+    //     <button onClick={() => dispatch(decrement())}>Azalt</button></div>
+    // </div>
+
+    <div>
+      <UserList></UserList>
+    </div>
+  )
+}
+
+export default App
